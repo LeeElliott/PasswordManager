00b5ede4444ce3fd30bb5a9e684876bcf640adfe
LeeElliott <35928612+LeeElliott@users.noreply.github.com>
2020-11-08 00:06:39 +0000

    Initial commit

---
+++ Assets/Scripts/LoginControllerScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoginControllerScript : MonoBehaviour
+{
+    public InputField inputField;
+
+    public void LoginClicked()
+    {
+        string enterredPassword = inputField.text;
+
+        if (PasswordCorrect(enterredPassword))
+        {
+            // Login passed initialise menu scene
+            SceneManager.LoadScene("Menu");
+        }
+    }
+
+    bool PasswordCorrect(string a)
+    {
+        // Look up stored password
+        string storedPassword = "NotSet";
+
+        // Input matches stored password
+        if (storedPassword == a)
+        {
+            // Check passed
+            return true;
+        }
+        else if ( storedPassword == "NotSet")
+        {
+            // Set stored password
+
+
+            // Check passed
+            return true;
+        }
+
+        // No match, check failed
+        return false;
+    }
+}
---
+++ Assets/Scripts/MenuControllerScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuControllerScript : MonoBehaviour {
+
+	public void ViewListClicked()
+    {
+        // Initialise the list viewer
+        SceneManager.LoadScene("ListViewer");
+    }
+
+    public void AddEntryClicked()
+    {
+        // Initialise new entry
+        SceneManager.LoadScene("AddEntry");
+    }
+}
